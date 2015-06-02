e1637f5aff2d8c23d25b326a2f961b4ce3858c83
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2015-06-01 20:26:24 -0700

    Install YCM dependencies in dotinstall.sh

--- dotinstall.sh
+++ dotinstall.sh
@@ -1,10 +1,21 @@
 #/usr/bin/env bash
 
+set -e
+
 git config status.showUntrackedFiles no
 git submodule update --init --recursive
 
 vim +PluginInstall +qall
 
+install=""
+for package in "build-essential" "cmake" "python-dev"; do
+    dpkg -s "$package" &> /dev/null || install="$install $package"
+done
+
+if [ -n "$install" ]; then
+    sudo apt-get install $install -y
+fi
+
+
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh --clang-completer
-
