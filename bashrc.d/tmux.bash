ff108fba350e7687a0f4fa0c26051eddeed7c35f
Jeffrey Faer <jeffrey.faer@gmail.com>
2024-03-26 13:19:36 -0600

    fix(tvs): Record tmux-vcs-sync update in history at the correct point.

--- bashrc.d/tmux.bash
+++ bashrc.d/tmux.bash
@@ -16,7 +16,14 @@ if [[ -n "$(command -v tmux-vcs-sync)" ]]; then
             return
         fi
         if tmux-vcs-sync update --fail-noop; then
+            # This is being executed in a pre-exec, so the command being
+            # executed is already in history. Let's rewrite history to include
+            # the tmux-vcs-sync call.
+            local last_cmd=()
+            eval "$(dotfiles::shlex last_cmd "$(fc -n -l -0)")"
+            history -d -1
             history -s tmux-vcs-sync update
+            history -s "${last_cmd[@]}"
         fi
     }
     preexec_functions+=("bashrc::tvs_preexec")
